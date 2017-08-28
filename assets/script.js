4c4795ff41958cfe77555d5cf5f5da68efe5e5e8
Anne-marie edna Wynne <845869352@cit3-501m-08.auckland.natcoll.ac.nz>
2017-08-28 15:41:58 +1200

    behance profile added

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,26 @@
 $(function(){
 
 
+	let key = 'Tt1i6BweiDbecwnbp4L1Bx87XJgm0bk4'
 
+	if($('#index').length>0){
+		let urlProjects = 'https://api.behance.net/v2/users/neo_innov/projects?client_id='+key;
 
+	$.ajax({
+			url: urlProjects,
+			dataType: 'jsonp',
+			success: function(res){
+				let projects = res.projects;
+				_(projects).each(function(project){
+					$('<li>'+project.name+'<img src="'+project.covers.original+'"><a href="project.html?projectid='+project.id+'">see more</a></li>')
+					.appendTo('ul.projects');
+				});
+			}
+			
+
+		});
+
+	}
 
 	
 	
