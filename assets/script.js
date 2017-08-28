4d77276d9cd280d864e821ca434e32fe0c962ccf
Anne-Marie Wynne <AM@nzlisdevs-Mac-mini.local>
2017-08-29 09:49:18 +1200

    updated script for image info

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,28 @@ $(function(){
 		});
 
 	}
+		if($('#project').length>0){
 
+		let pageURL = new URL(document.location);
+		let params = pageURL.searchParams;
+		let projectid = params.get('projectid');
+
+		let urlProject = 'http://www.behance.net/v2/projects/'+projectid+'?api_key='+key;
+
+		$.ajax({
+			url: urlProject,
+			dataType: 'jsonp',
+			success: function(res){
+				let project = res.project;
+
+				$('<h1>'+project.name+'</h1>').appendTo('.container');
+				$('<p>'+project.description+'</p>').appendTo('.container');
+				$('<h3>'+moment.unix(project.published_on).fromNow()+'</h3>').appendTo('.container');
+				$('<img src="'+project.covers.original+'">').appendTo('.container');
+			}
+		});
+
+	}
 	
 	
 });
