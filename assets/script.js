6b6398e42b336bfc82547f282c93a32a02bfdaa9
Anne-marie edna Wynne <845869352@cit3-501m-08.auckland.natcoll.ac.nz>
2017-08-29 15:12:37 +1200

    image styling to main page

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,40 @@ $(function(){
 	let key = 'Tt1i6BweiDbecwnbp4L1Bx87XJgm0bk4'
 
 	if($('#index').length>0){
-		let urlProjects = 'https://api.behance.net/v2/users/neo_innov/projects?client_id='+key;
 
-	$.ajax({
+		//user
+		let urlUser = 'https://api.behance.net/v2/users/neo_innov?client_id='+key;
+		$.ajax({
+			url: urlUser,
+			dataType: 'jsonp',
+			success: function(res){
+				let user = res.user;
+				
+				console.log();
+				$('<h1>'+user.display_name+'</h1>').appendTo('.header-img');
+				$('<h3>'+user.location+'</h3>').appendTo('.header-img');
+				$('<h4>'+user.occupation+'</h4>').appendTo('.header-img');
+				// $('<h3>'+user.username+'</h3>').appendTo('.header-img');
+
+			}
+			
+		});
+
+		//projects
+		let urlProjects = 'https://api.behance.net/v2/users/neo_innov/projects?client_id='+key;
+		$.ajax({
 			url: urlProjects,
 			dataType: 'jsonp',
 			success: function(res){
 				let projects = res.projects;
 				_(projects).each(function(project){
-					$('<li>'+project.name+'<img src="'+project.covers.original+'"><a href="project.html?projectid='+project.id+'">see more</a></li>')
+					$('<li><img src="'+project.covers.original+'"><p>'+project.name+'</p><a href="project.html?projectid='+project.id+'">see more</a><i class="fa fa-thumbs-up" aria-hidden="true"></i><p>'+project.stats.views+'</p></li>')
 					.appendTo('ul.projects');
 				});
 			}
 			
-
 		});
+
 
 	}
 		if($('#project').length>0){
@@ -35,10 +54,11 @@ $(function(){
 			success: function(res){
 				let project = res.project;
 
+				$('<img src="'+project.covers.original+'">').appendTo('.container');
 				$('<h1>'+project.name+'</h1>').appendTo('.container');
 				$('<p>'+project.description+'</p>').appendTo('.container');
 				$('<h3>'+moment.unix(project.published_on).fromNow()+'</h3>').appendTo('.container');
-				$('<img src="'+project.covers.original+'">').appendTo('.container');
+				
 			}
 		});
 
